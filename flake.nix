1a0bd7ba6311139b744bcf544183545fb925ff65
MayNiklas <info@niklas-steffen.de>
2023-08-03 22:41:59 +0200

    add formatter to flake.nix

--- flake.nix
+++ flake.nix
@@ -8,14 +8,38 @@
       url = github:nix-community/home-manager;
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    # https://github.com/numtide/flake-utils
+    # flake-utils provides a set of utility functions for creating multi-output flakes
+    # -> lets us easily define a output for different system types
+    # -> could be replaced by a more manual approach
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self,  ... }@inputs:
-  let
-    system = "x86_64-linux";
-  in
-  with inputs;
-  {
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in
+    with inputs;
+    # this function is used to repeat the same definitions for multible architectures
+    (flake-utils.lib.eachSystem (flake-utils.lib.defaultSystems))
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config = {
+              allowUnfree = true;
+              allowUnsupportedSystem = true;
+            };
+          };
+        in
+        rec {
+          # nix fmt .
+          formatter = pkgs.nixpkgs-fmt;
+        }
+      )
+    //
+    {
     nixosConfigurations = {
       nixos_portable = nixpkgs.lib.nixosSystem {
         inherit system;
