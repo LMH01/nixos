495feec3c48b52e2ffa6a7e2c0eb0317b7e0eb07
LMH01 <=>
2023-08-18 23:29:04 +0200

    Changed vscode config

--- home-manager/modules/vscode/default.nix
+++ home-manager/modules/vscode/default.nix
@@ -6,15 +6,26 @@
     enableUpdateCheck = false;
     enableExtensionUpdateCheck = false;
     extensions = with pkgs.vscode-extensions; [
-        dracula-theme.theme-dracula
-        redhat.java
-        rust-lang.rust-analyzer
-        streetsidesoftware.code-spell-checker
-        tamasfe.even-better-toml
-        usernamehw.errorlens
-        vadimcn.vscode-lldb
-        vscodevim.vim
+      b4dm4n.vscode-nixpkgs-fmt
+      dracula-theme.theme-dracula
+      jnoortheen.nix-ide
+      redhat.java
+      rust-lang.rust-analyzer
+      streetsidesoftware.code-spell-checker
+      tamasfe.even-better-toml
+      usernamehw.errorlens
+      vadimcn.vscode-lldb
+      vscodevim.vim
     ];
+
+    userSettings = {
+      "workbench.colorTheme" = "Dracula";
+      "nix.enableLanguageServer" = "true";
+      "nix.serverPath" = "${pkgs.nil}/bin/nil";
+      "[nix]" = {
+        "editor.defaultFormatter" = "B4dM4n.nixpkgs-fmt";
+      };
+    };
   };
 
 }
