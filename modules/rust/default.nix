dafcc6028fe75a89e074729ed4b8c47ac21c0c69
LMH01 <louis.haensch01@gmail.com>
2023-09-05 00:59:35 +0200

    Added gcc to rust module

--- modules/rust/default.nix
+++ modules/rust/default.nix
@@ -10,7 +10,9 @@ in
 
   config = mkIf cfg.enable {
 
-    environment.systemPackages = with pkgs; [ cargo rustc clippy ];
+    # TODO see if this is feasable or if it is a better idea to use rust using a nix shell
+    # (i heared that it is a bad idea to install gcc directly this way)
+    environment.systemPackages = with pkgs; [ cargo rustc clippy gcc ];
 
   };
 }
