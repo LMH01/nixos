4d32cc2612b3f33bfb9f9e363784fff8640a2b24
LMH01 <louis.haensch01@gmail.com>
2023-09-13 17:09:04 +0200

    Added prismlauncher to main desktop config

--- home-manager/profiles/CBPC-0123_LMH.nix
+++ home-manager/profiles/CBPC-0123_LMH.nix
@@ -7,6 +7,7 @@ with lib;
 
     home.packages = with pkgs; [
       hashcat
+      prismlauncher # minecraft launcher
       steam
     ] ++ lib.optionals (system-config.nixpkgs.hostPlatform.system == "x86_64-linux") [ ];
 
