d96e6d6c290ce209387fa7bcd88c74b26e2c9dc7
LMH01 <lmh-01@netcologne.de>
2023-09-21 10:23:04 +0200

    Added parsec to portable config

--- home-manager/profiles/portable.nix
+++ home-manager/profiles/portable.nix
@@ -6,7 +6,7 @@ with lib;
   config = {
 
     home.packages = with pkgs; [
-
+      parsec-bin
     ] ++ lib.optionals (system-config.nixpkgs.hostPlatform.system == "x86_64-linux") [ ];
 
     programs = {
