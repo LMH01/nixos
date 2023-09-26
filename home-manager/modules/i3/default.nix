352d1c51155f6223fc8f1f2c72071cf06f35e5c8
LMH01 <louis.haensch01@gmail.com>
2023-09-26 09:12:06 +0200

    Changed i3bar font to FontAwesome

--- home-manager/modules/i3/default.nix
+++ home-manager/modules/i3/default.nix
@@ -49,7 +49,7 @@ in
 
         bars = [
           {
-            fonts = ["Iosevka 11"];
+            fonts = ["FontAwesome 11"];
             position = "top";
             statusCommand = "${pkgs.i3status-rust}/bin/i3status-rs ~/.config/i3status-rust/config-top.toml";
           }
