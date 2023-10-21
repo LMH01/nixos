9816ce159c6dd43f3720921bee638aac28afc598
LMH01 <louis.haensch01@gmail.com>
2023-10-21 22:51:28 +0200

    Added tmux to server config

--- home-manager/profiles/server.nix
+++ home-manager/profiles/server.nix
@@ -6,7 +6,7 @@ with lib;
   config = {
 
     home.packages = with pkgs; [
-
+      tmux
     ] ++ lib.optionals (system-config.nixpkgs.hostPlatform.system == "x86_64-linux") [ ];
 
     programs = { };
