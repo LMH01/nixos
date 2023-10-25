0383d925c8ef44fbcd4e281808efb19dcb708535
LMH01 <louis.haensch01@gmail.com>
2023-10-25 16:49:31 +0200

    Added pentesting module util alinkbetweennets' module works

--- home-manager/profiles/laptop.nix
+++ home-manager/profiles/laptop.nix
@@ -2,9 +2,13 @@
 { lib, pkgs, flake-self, config, system-config, ... }:
 with lib;
 {
+  imports = with flake-self.homeManagerModules; [
+    pentesting
+  ];
 
   config = {
 
+    lmh01.pentesting.enable = true;
     home.packages = with pkgs; [
 
     ] ++ lib.optionals (system-config.nixpkgs.hostPlatform.system == "x86_64-linux") [ ];
