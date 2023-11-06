29f678ce1512cb13854de58dd72c8484fecba214
LMH01 <lmh-01@netcologne.de>
2023-11-06 15:05:56 +0100

    Fixed wireguard config

--- modules/wg-sn/default.nix
+++ modules/wg-sn/default.nix
@@ -12,11 +12,11 @@ in {
 
       wg-quick.interfaces = {
         wg-sn = {
-          address = [ "10.0.1.2/24" "fdc9:281f:04d7:9eea::2/64" ];
+          address = [ "10.0.1.3/24" "fdc9:281f:04d7:9eea::3/64" ];
           privateKeyFile = "${config.lmh01.secrets}/wg-sn-louis.private";
           peers = [
             {
-              publicKey = "p3ZnR7XTY8dO+zmUzMQUjVQHQwWFxIUTu9jvcuuUrQc=";
+              publicKey = "9Hn/0/npzyZ+afzk0ux5oDvqjsbgLrrU9UC7qij13yE=";
               presharedKeyFile = "${config.lmh01.secrets}/wg-sn-louis.preshared";
               allowedIPs = [ "10.0.1.0/24" "fdc9:281f:04d7:9eea::1/64" ];
               endpoint = "alinkbetweennets.de:51820";
