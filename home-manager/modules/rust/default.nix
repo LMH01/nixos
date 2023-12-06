0581a707a1344689d89b2d67a226d5779b8ebef5
LMH01 <louis.haensch01@gmail.com>
2023-12-06 10:08:47 +0100

    Added crates extension to vscode when rust is enabled

--- home-manager/modules/rust/default.nix
+++ home-manager/modules/rust/default.nix
@@ -18,6 +18,9 @@ in
       rustfmt
     ];
 
-    programs.vscode.extensions = with pkgs.vscode-extensions; [ rust-lang.rust-analyzer ];
+    programs.vscode.extensions = with pkgs.vscode-extensions; [ 
+      rust-lang.rust-analyzer 
+      serayuzgur.crates
+    ];
   };
 }
