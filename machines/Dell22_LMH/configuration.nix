236e0f1dbfec3905c6bde499e2f9b87333483bb5
LMH01 <lmh-01@netcologne.de>
2024-02-29 17:58:02 +0100

    updated kde plasma to 6

--- machines/Dell22_LMH/configuration.nix
+++ machines/Dell22_LMH/configuration.nix
@@ -74,7 +74,7 @@
 
   # Enable the KDE Plasma Desktop Environment.
   services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.desktopManager.plasma6.enable = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
