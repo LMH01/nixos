813f19e45d729c1b1fd43493f3285c2283c3f92c
LMH01 <louis.haensch01@gmail.com>
2024-07-21 17:43:20 +0200

    removed deprecated sound option

--- machines/nixos_portable/configuration.nix
+++ machines/nixos_portable/configuration.nix
@@ -73,7 +73,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
