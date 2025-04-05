6a7efe553587094fb47f636a6e9beee7671d62c3
LMH01 <louis.haensch01@gmail.com>
2025-04-05 21:29:14 +0200

    added base config for hsarr

---
+++ machines/hsarr/configuration.nix
@@ -0,0 +1,54 @@
+# nix run .\#lollypops -- pi4b
+{ self, ... }:
+{ pkgs, lib, config, modulesPath, flake-self, home-manager, nixos-hardware, nixpkgs, ... }: {
+
+  imports = [
+    ./hardware-configuration.nix
+    home-manager.nixosModules.home-manager
+
+    # this machine is a server
+    self.nixosModules.server
+
+  ];
+
+  # this workaround is currently needed to build the sd-image
+  # basically: there currently is an issue that prevents the sd-image to be built successfully
+  # remove this once the issue is fixed!
+  nixpkgs.overlays = [
+    (final: super: {
+      makeModulesClosure = x:
+        super.makeModulesClosure (x // { allowMissing = true; });
+    })
+  ];
+  ###
+
+  lmh01 = {
+    options.type = "server";
+  };
+
+  # Home Manager configuration
+  home-manager = {
+    # DON'T set useGlobalPackages! It's not necessary in newer
+    # home-manager versions and does not work with configs using
+    # nixpkgs.config
+    useUserPackages = true;
+    extraSpecialArgs = {
+      inherit flake-self;
+      # Pass system configuration (top-level "config") to home-manager modules,
+      # so we can access it's values for conditional statement
+      system-config = config;
+    };
+    users.louis = flake-self.homeConfigurations.server;
+  };
+
+  networking.hostName = "hsarr";
+
+  networking.networkmanager.enable = true;
+  networking.networkmanager.plugins = lib.mkForce [ ];
+
+  networking.firewall.allowedTCPPorts = [ ];
+
+  networking.firewall.allowedUDPPorts = [ ];
+
+  system.stateVersion = "23.05";
+}
---
+++ machines/hsarr/disk-config.nix
@@ -0,0 +1,57 @@
+{
+  disko.devices = {
+    disk = {
+      sda = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              priority = 1;
+              size = "1M";
+              type = "EF02";
+            };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ];
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "/home" = {
+                    mountpoint = "/home";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [ "compress=zstd" "noatime" ];
+                  };
+                  "/swap" = {
+                    mountpoint = "/.swapvol";
+                    swap.swapfile.size = "32G";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
