191838365b9366541adae371cc8b63c030789496
LMH01 <lmh-01@netcologne.de>
2025-06-19 13:59:43 +0200

    tried adding ssl to nginx for nextcloud

--- modules/nextcloud/default.nix
+++ modules/nextcloud/default.nix
@@ -1,6 +1,7 @@
 { config, pkgs, lib, ... }:
 with lib;
 let cfg = config.lmh01.nextcloud;
+# this module is currently experimental
 in {
   options.lmh01.nextcloud.enable = mkEnableOption "activate nextcloud";
   config = mkIf cfg.enable {
@@ -8,17 +9,35 @@ in {
       "nextcloud/password" = {
         owner = "louis";
       };
+      "nginx/dhparam" = {
+        owner = "nginx";
+      };
+      "nginx/sslCertificate" = {
+        owner = "nginx";
+      };
+      "nginx/sslCertificateKey" = {
+        owner = "nginx";
+      };
     };
     services.nextcloud = {
       enable = true;
       package = pkgs.nextcloud31;
       hostName = "localhost";
       phpExtraExtensions = all: [ all.smbclient ];
-      config = { 
+      config = {
         adminpassFile = config.sops.secrets."nextcloud/password".path;
         dbtype = "sqlite";
       };
+      https = true;
     };
-    services.nginx.virtualHosts."localhost".listen = [{ addr = "127.0.0.1"; port = 11808; }];
+    services.nginx = {
+      virtualHosts."localhost" = {
+        listen = [{ addr = "127.0.0.1"; port = 11808; }];
+        addSSL = true;
+        sslCertificate = config.sops.secrets."nginx/sslCertificate".path;
+        sslCertificateKey = config.sops.secrets."nginx/sslCertificateKey".path;
+      };
+      sslDhparam = config.sops.secrets."nginx/dhparam".path;
+    };
   };
 }
