130f9dfe76b77e01cc2a0da20190d248906662ab
LMH01 <lmh-01@netcologne.de>
2025-12-18 18:44:43 +0100

    add header settings to nginx minerva

--- machines/minerva/configuration.nix
+++ machines/minerva/configuration.nix
@@ -59,6 +59,14 @@
         useACMEHost = "${config.lmh01.domain}";
         locations."/" = {
           proxyPass = "https://jellyfin.home.skl2.de";
+          extraConfig = ''
+            proxy_set_header    Upgrade           $http_upgrade;
+            proxy_set_header    Connection        "upgrade";
+            proxy_set_header    Host              $host;
+            proxy_set_header    X-Real-IP         $remote_addr;
+            proxy_set_header    X-Forwarded-For   $proxy_add_x_forwarded_for;
+            proxy_set_header    X-Forwarded-Proto $scheme; 
+          '';
         };
       };
       "jellyseer.${config.lmh01.domain}" = {
